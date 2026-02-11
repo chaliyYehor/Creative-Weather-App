a7c5fbe07eabe44b79f56f6d85f8219abd346c76
Yehor <chaliyegor00@gmail.com>
2026-02-11 21:49:49 +0200

    add a lang prop to the queryOptions func

--- src/queryOptions/weatherQueryOptions.ts
+++ src/queryOptions/weatherQueryOptions.ts
@@ -3,15 +3,21 @@ import { queryOptions } from '@tanstack/react-query'
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
-export default function autocompleteQueryOptions(search: string) {
+export default function autocompleteQueryOptions(
+	search: string,
+	lang: 'uk' | 'en',
+) {
 	return queryOptions({
-		queryKey: ['autocomplete', search],
-		queryFn: () => fetchSuggestions(search),
+		queryKey: ['autocomplete', search, lang],
+		queryFn: () => fetchSuggestions(search, lang),
 		enabled: !!search,
 	})
 }
 
-async function fetchSuggestions(search: string): Promise<ForecastResponse> {
+async function fetchSuggestions(
+	search: string,
+	lang: 'uk' | 'en',
+): Promise<ForecastResponse> {
 	const res = await fetch(
 		`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${search}&days=7&aqi=no&alerts=yes&lang=en
 `,
