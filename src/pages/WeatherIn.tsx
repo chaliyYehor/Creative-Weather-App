14cb74b1ddd9854108623899ba6b79fabb7ea21d
Yehor <chaliyegor00@gmail.com>
2026-02-11 18:01:22 +0200

    get the data useing useQuery hook and WeatherqueryOptions

--- src/pages/WeatherIn.tsx
+++ src/pages/WeatherIn.tsx
@@ -1,6 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import weatherQueryOptions from '#queryOptions/weatherQueryOptions'
+import { useEffect } from 'react'
+
 const WeatherIn = () => {
+	const { city } = useParams()
+	const { data, isPending } = useQuery(weatherQueryOptions(city as string))
+
+	useEffect(() => {
+		console.log(data)
+	}, [data])
+
 	return (
-		<div>WeatherIn</div>
+		<div className='text-5xl text-white'>
+			{isPending ? 'pending' : 'hell nah'}
+		</div>
 	)
 }
 
