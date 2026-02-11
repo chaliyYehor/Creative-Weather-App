87a698a95f32a04a6ad3ead649c8dff8e7321b78
Yehor <chaliyegor00@gmail.com>
2026-02-11 18:17:21 +0200

    add a loading anim from redirecting

--- src/pages/WeatherIn.tsx
+++ src/pages/WeatherIn.tsx
@@ -1,20 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import weatherQueryOptions from '#queryOptions/weatherQueryOptions'
-import { useEffect } from 'react'
+import FadeOut from '#components/FadeOut'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const WeatherIn = () => {
 	const { city } = useParams()
-	const { data, isPending } = useQuery(weatherQueryOptions(city as string))
+	const { data, isFetched } = useQuery(weatherQueryOptions(city as string))
 
-	useEffect(() => {
-		console.log(data)
-	}, [data])
+	useGSAP(() => {
+		if (isFetched) {
+			gsap.from('.slicesWrapper div', {
+				x: 0,
+				duration: 1,
+				stagger: 0.1,
+				delay: 0.3,
+				ease: 'power1.inOut',
+			})
+		}
+	}, [isFetched])
 
 	return (
-		<div className='text-5xl text-white'>
-			{isPending ? 'pending' : 'hell nah'}
-		</div>
+		<>
+			<FadeOut />
+		</>
 	)
 }
 
