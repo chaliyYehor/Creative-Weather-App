470a4195e8dafd8f3d9b8e85246cbf0bd9791ff4
Yehor <chaliyegor00@gmail.com>
2026-02-13 14:49:43 +0200

    connect autocomplete service to the store itself, setupListeners and set up middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import langSliceReducer from '#store/slices/languageSlice'
+import { autocompleteApi } from './services/autocompleteApi'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
 	reducer: {
 		langSlice: langSliceReducer,
+		[autocompleteApi.reducerPath]: autocompleteApi.reducer,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(autocompleteApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootType = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
