b8e94a45839a03702ada053a34f99d1dda919361
Yehor <chaliyegor00@gmail.com>
2026-02-15 17:20:15 +0200

    add more conditions for snowy weather in constants.ts and implement redirecting functionality for RV cards after clicking

--- src/components/RecentlyViewedCard.tsx
+++ src/components/RecentlyViewedCard.tsx
@@ -2,22 +2,44 @@ import type { RootType } from '#store/store'
 import { useQuery } from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
 import weatherQueryOptions from '#queryOptions/weatherQueryOptions'
+import { useNavigate } from 'react-router-dom'
+import gsap from 'gsap'
 
 type RecentlyViewedCardProps = {
 	place: string
 }
 
 const RecentlyViewedCard = ({ place }: RecentlyViewedCardProps) => {
+	const navigate = useNavigate()
 	const lang = useSelector((state: RootType) => state.langSlice.lang)
 
 	const city = place.includes('&') ? place.split('&')[1] : place
 
-	const { data, isFetched } = useQuery(
-		weatherQueryOptions(city as string, lang),
-	)
+	async function getMoreInfo() {
+		;(document.activeElement as HTMLElement)?.blur()
+
+		gsap.to('.slicesWrapper div', {
+			x: 0,
+			duration: 1,
+			pointerEvents: 'all',
+			stagger: 0.1,
+			delay: 0.3,
+			ease: 'power1.inOut',
+		})
+
+		//sleepTimeForTheAnimation
+		await new Promise(res => setTimeout(res, 1500))
+		
+		const latLon = place.includes('&') && place.split('&')[1]
+		const properUrl = place.includes('&') ? `${latLon}&${place.split('&')[0]}` : place
+
+		navigate(`/weatherIn/${properUrl}`)
+	}
+
+	const { data } = useQuery(weatherQueryOptions(city as string, lang))
 
 	return (
-		<div className='cardWrapper border-2 border-white/30 w-full h-20 rounded-sm text-2xl hover:border-white/50 transition grid gap-2 grid-cols-[60%_1fr_1fr]'>
+		<div onClick={() => getMoreInfo()} className='cardWrapper border-2 border-white/30 w-full h-20 rounded-sm text-2xl hover:border-white/50 transition grid gap-2 grid-cols-[60%_1fr_1fr]'>
 			<div className='city flex items-center gap-5 pl-5 w-full h-full'>
 				<h3>{place.includes('&') ? place.split('&')[0] : place}</h3>
 			</div>
