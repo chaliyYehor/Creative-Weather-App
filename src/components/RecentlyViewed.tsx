d46081d8b4181e2786ce1eddea51b69aef94c933
Yehor <chaliyegor00@gmail.com>
2026-02-15 15:14:40 +0200

    render a trash can icon that will clear all saved places and add styles for the containers of Recently viewed and recently viewed cards

--- src/components/RecentlyViewed.tsx
+++ src/components/RecentlyViewed.tsx
@@ -1,9 +1,56 @@
-import { forwardRef } from 'react'
+import { Trash } from 'lucide-react'
+import RecentlyViewedCard from './RecentlyViewedCard'
 
-const RecentlyViewed = forwardRef<HTMLDivElement>((_, ref) => {
+const RecentlyViewed = () => {
+	const places = [
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+		'London',
+		'Bila Tserkva',
+		'Rome',
+	]
+
 	return (
-		<div>RecentlyViewed</div>
+		<>
+			<div className='recentlyViewedWrapper text-white w-100 flex flex-col items-center mt-20 max-h-100 overflow-hidden gap-8 relative'>
+				<h3 className='text-3xl'>Recently Viewed</h3>
+
+				<div className="clearAll text-white/50 hover:text-white transition w-fit h-fit absolute right-0 top-2 cursor-pointer" title='Clear all'>
+					<Trash />
+				</div>
+
+				<div className='listWrapper h-full overflow-auto w-full'>
+					<ul className='flex flex-col gap-2'>
+						{places.map((place, index) => (
+							<RecentlyViewedCard key={index} text={place} />
+						))}
+					</ul>
+				</div>
+			</div>
+		</>
 	)
-})
+}
 
 export default RecentlyViewed
--- src/components/RecentlyViewedCard.tsx
+++ src/components/RecentlyViewedCard.tsx
@@ -1,6 +1,12 @@
-const RecentlyViewedCard = () => {
+type RecentlyViewedCardProps = {
+	text: string
+}
+
+const RecentlyViewedCard = ({ text }: RecentlyViewedCardProps) => {
 	return (
-		<div>RecentlyViewedCard</div>
+		<div className='cardWrapper border-2 border-white/30 w-full h-20 rounded-sm text-2xl hover:border-white/50 transition'>
+			{text}
+		</div>
 	)
 }
 
