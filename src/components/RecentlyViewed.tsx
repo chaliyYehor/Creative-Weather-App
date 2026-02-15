992eab36b2f51812df06233c98c78a7e79e2c5a1
Yehor <chaliyegor00@gmail.com>
2026-02-15 14:00:48 +0200

    create a component for recently viewed places

---
+++ src/components/RecentlyViewed.tsx
@@ -0,0 +1,9 @@
+import { forwardRef } from 'react'
+
+const RecentlyViewed = forwardRef<HTMLDivElement>((_, ref) => {
+	return (
+		<div>RecentlyViewed</div>
+	)
+})
+
+export default RecentlyViewed
